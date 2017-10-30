de5a4bf4048b4090db62fae005c520e9d7239433
Ethan Sutherlannd <ESutherland95@gmail.com>
2017-10-30 08:59:53 -0500

    Initial commit

---
+++ scripts/recipeProject.js
@@ -0,0 +1,122 @@
+function defaultChiliValues() {
+    $('#oilAmtChili').html('2');
+    $('#onionAmtChili').html('1');
+    $('#peppersAmtChili').html('1');
+    $('#chiliAmtChili').html('8');
+    $('#hotChiliAmtChili').html('2');
+    $('#meatAmtChili').html('1');
+    $('#redAmtChili').html('2');
+    $('#kidneyAmtChili').html('2');
+    $('#tomatoAmtChili').html('2');
+    $('#sauceAmtChili').html('2');
+    $('#cheeseAmtChili').html('1');
+    $('#creamAmtChili').html('1/2');
+
+    $('#panQuartChili').html('2');
+    $('#crockQuartChili').html('3');
+    $('#cookTimeChili').html('6');
+}
+
+function halfChiliValues() {
+    $('#oilAmtChili').html('1');
+    $('#onionAmtChili').html('1/2');
+    $('#peppersAmtChili').html('1/2');
+    $('#chiliAmtChili').html('4');
+    $('#hotChiliAmtChili').html('1');
+    $('#meatAmtChili').html('1/2');
+    $('#redAmtChili').html('1');
+    $('#kidneyAmtChili').html('1');
+    $('#tomatoAmtChili').html('1');
+    $('#sauceAmtChili').html('1');
+    $('#cheeseAmtChili').html('1/2');
+    $('#creamAmtChili').html('1/4');
+
+    $('#panQuartChili').html('1');
+    $('#crockQuartChili').html('1-1/2');
+    $('#cookTimeChili').html('3');
+}
+
+function doubleChiliValues() {
+    $('#oilAmtChili').html('4');
+    $('#onionAmtChili').html('2');
+    $('#peppersAmtChili').html('2');
+    $('#chiliAmtChili').html('16');
+    $('#hotChiliAmtChili').html('4');
+    $('#meatAmtChili').html('2');
+    $('#redAmtChili').html('4');
+    $('#kidneyAmtChili').html('4');
+    $('#tomatoAmtChili').html('4');
+    $('#sauceAmtChili').html('4');
+    $('#cheeseAmtChili').html('2');
+    $('#creamAmtChili').html('1');
+
+    $('#panQuartChili').html('4');
+    $('#crockQuartChili').html('6');
+    $('#cookTimeChili').html('12');
+}
+
+function hideAll() {
+    $('#recipe1').hide();
+    $('#recipe2').hide();
+    $('#recipe3').hide();
+}
+
+function defaultSoupValues() {
+    $('#beefAmtSoup').html('1/2');
+    $('#butterAmtSoup').html('4');
+    $('#onionAmtSoup').html('3/4');
+    $('#carrotAmtSoup').html('3/4');
+    $('#celeryAmt').html('3/4');
+    $('#basilAmtSoup').html('1');
+    $('#parsleyAmtSoup').html('1');
+    $('#potatoAmtSoup').html('1-3/4');
+    $('#brothAmtSoup').html('3');
+    $('#flourAmtSoup').html('1/4');
+    $('#cheeseAmtSoup').html('1');
+    $('#milkAmtSoup').html('1-1/2');
+    $('#saltAmtSoup').html('3/4');
+    $('#pepperAmtSoup').html('1/4');
+    $('#creamAmtSoup').html('1/4');
+
+    $('#meltButterAmtSoup').html('1');
+}
+
+function halfSoupValues() {
+    $('#beefAmtSoup').html('1/4');
+    $('#butterAmtSoup').html('2');
+    $('#onionAmtSoup').html('3/8');
+    $('#carrotAmtSoup').html('3/8');
+    $('#celeryAmt').html('3/8');
+    $('#basilAmtSoup').html('1/2');
+    $('#parsleyAmtSoup').html('1/2');
+    $('#potatoAmtSoup').html('7/8');
+    $('#brothAmtSoup').html('1-1/2');
+    $('#flourAmtSoup').html('1/8');
+    $('#cheeseAmtSoup').html('1/2');
+    $('#milkAmtSoup').html('3/4');
+    $('#saltAmtSoup').html('3/8');
+    $('#pepperAmtSoup').html('1/8');
+    $('#creamAmtSoup').html('1/8');
+
+    $('#meltButterAmtSoup').html('1/2');
+}
+
+function doubleSoupValues() {
+    $('#beefAmtSoup').html('1');
+    $('#butterAmtSoup').html('8');
+    $('#onionAmtSoup').html('1-1/2');
+    $('#carrotAmtSoup').html('1-1/2');
+    $('#celeryAmt').html('1-1/2');
+    $('#basilAmtSoup').html('2');
+    $('#parsleyAmtSoup').html('2');
+    $('#potatoAmtSoup').html('3-1/2');
+    $('#brothAmtSoup').html('6');
+    $('#flourAmtSoup').html('1/2');
+    $('#cheeseAmtSoup').html('2');
+    $('#milkAmtSoup').html('3');
+    $('#saltAmtSoup').html('1-1/2');
+    $('#pepperAmtSoup').html('1/2');
+    $('#creamAmtSoup').html('1/2');
+
+    $('#meltButterAmtSoup').html('2');
+}
